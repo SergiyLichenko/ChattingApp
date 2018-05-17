25a2eedb8d996cfcf34f347feda3f7a3b5e48353
SergiyLichenko <sergiy.lichenko@outlook.com>
2018-05-17 23:27:00 +0300

    Updated dependencies

---
+++ ChattingApp.Repository/AuthContext.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using ChattingApp.Repository.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ChattingApp.Repository
+{
+    public class AuthContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>().HasKey(x => x.Id);
+            modelBuilder.Entity<ApplicationUser>().HasMany(x => x.Chats).WithMany(x => x.Users);
+        }
+    }
+
+    public class MyInitializer : DropCreateDatabaseIfModelChanges<AuthContext>
+    {
+        protected override void Seed(AuthContext context)
+        {
+            context.Roles.Add(new IdentityRole("User"));
+            context.Roles.Add(new IdentityRole("Moderator"));
+            context.Roles.Add(new IdentityRole("Admin"));
+            context.SaveChanges();
+
+            if (context.Clients.Any()) return;
+            context.Clients.AddRange(BuildClientsList());
+            context.SaveChanges();
+            base.Seed(context);
+        }
+
+        private static List<Client> BuildClientsList()
+        {
+
+            List<Client> ClientsList = new List<Client>
+            {
+                new Client
+                { Id = "SMARTMessenger",
+                    Secret= EncryptPassword.GetEncryptedPassword("WebMessengerSecret@Smart2016"),
+                    Name="AngularJS front-end Application",
+                    ApplicationType =  ApplicationTypes.JavaScript,
+                    Active = true,
+                    RefreshTokenLifeTime = 7200,
+                    AllowedOrigin = "*"
+                },
+                new Client
+                { Id = "consoleApp",
+                    Secret=EncryptPassword.GetEncryptedPassword("DesktopMessengerSecret@Smart2016"),
+                    Name="Console Application",
+                    ApplicationType =ApplicationTypes.NativeConfidential,
+                    Active = true,
+                    RefreshTokenLifeTime = 14400,
+                    AllowedOrigin = "*"
+                }
+            };
+
+            return ClientsList;
+        }
+    }
+}
---
+++ ChattingApp/AngularPart/app.js
@@ -0,0 +1,75 @@
+﻿var app = angular.module("ChattingApp",
+    ["LocalStorageModule",
+        "ui.bootstrap",
+        "ui.router",
+        "validation.match",
+        "cgBusy",
+        "angularFileUpload"]);
+//, "", 'ngSanitize',
+//    '', '', '', '', '', 'ng.httpLoader']);
+
+app.run(["$templateCache",
+  function ($templateCache) {
+      $templateCache.put("background.html");
+  }
+]);
+app.config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            controller: 'LogInController',
+            templateUrl: 'AngularPart/Views/LogIn.html',
+            navbarState: 'login'
+        })
+        .state('home', {
+            url: '/home',
+            controller: 'HomeController',
+            templateUrl: 'AngularPart/Views/Home.html',
+            navbarState: 'home'
+        })
+        .state('signup', {
+            url: '/signup',
+            controller: 'SignUpController',
+            templateUrl: 'AngularPart/Views/SignUp.html',
+            navbarState: 'signup'
+        })
+        .state('chats', {
+            url: '/chats',
+            reloadOnSearch: true,
+            controller: 'ChatsController',
+            templateUrl: 'AngularPart/Views/Chats.html',
+            navbarState: 'chats'
+        })
+        .state('associate', {
+            url: '/associate',
+            controller: 'AssociateController',
+            templateUrl: 'AngularPart/Views/Association.html',
+            navbarState: 'associate'
+        })
+         .state('profile', {
+             url: '/profile',
+             controller: 'ProfileController',
+             templateUrl: 'AngularPart/Views/Profile.html',
+             navbarState: 'profile'
+         })
+        .state("otherwise",
+        {
+            url: "*path",
+            controller: 'HomeController',
+            templateUrl: 'AngularPart/Views/Home.html',
+            navbarState: 'home'
+        });
+}]);
+
+app.constant('webMessengerSettings', {
+    apiServiceBaseUri: '',
+    clientId: 'SMARTMessenger'
+});
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptorService');
+});
+
+app.run(["authService", function (authService) {
+    authService.fillAuthData();
+}]);
